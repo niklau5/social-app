8d10bcadf131f53daf8181ce2cdd4a1b0d46a339
Nikolai Laursen <nbl-brix@hotmail.com>
2017-03-14 13:53:08 +0100

    Sign in ui custom classes

---
+++ social-app/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  social-app
+//
+//  Created by Nikolai Brix Laursen on 14/03/2017.
+//  Copyright © 2017 CrewNET IVS. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY:CGFloat = 120.0/255.0
