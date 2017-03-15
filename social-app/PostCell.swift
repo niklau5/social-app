6ae47cd6873f1958de8d79753a285499c1ebd502
Nikolai Laursen <nbl-brix@hotmail.com>
2017-03-15 09:25:25 +0100

    feed table

---
+++ social-app/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  social-app
+//
+//  Created by Nikolai Brix Laursen on 15/03/2017.
+//  Copyright © 2017 CrewNET IVS. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    
+    
+    
+    
+
+  
+}
